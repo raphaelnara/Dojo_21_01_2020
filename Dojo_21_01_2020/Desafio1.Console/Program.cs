7f8c131a9f3d36673760c3d4020a52efddf56ce9
Raphael Nara Pereira <rnperei@crivo.com.br>
2020-01-17 12:00:35 -0300

    console para desafio 1

---
+++ Dojo_21_01_2020/Desafio1.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Desafio1.Services;
+
+namespace Desafio1.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta = null;
+            do
+            {
+                System.Console.WriteLine();
+
+                var partida = PartidaFactory.ContruirPartida();
+                PingPongService.Jogar(partida);
+
+                System.Console.WriteLine("Deseja jogar mais uma partida? (S/N)");
+                resposta = System.Console.ReadLine();
+            }
+            while (string.IsNullOrEmpty(resposta) || resposta.Equals("S", StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
--- Dojo_21_01_2020/Desafio1.Sumula/Program.cs
+++ Dojo_21_01_2020/Desafio1.Sumula/Program.cs
@@ -3,20 +3,5 @@ using Desafio1.Services;
 
 namespace Desafio1.Sumula
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var resposta = "S";
-            do
-            {
-                var partida = PartidaFactory.ContruirPartida();
-                PingPongService.Jogar(partida);
-                
-                Console.WriteLine("Deseja jogar mais uma partida? (S/N)");
-                resposta = Console.ReadLine();
-            }
-            while (!string.IsNullOrEmpty(resposta) && !resposta.Equals("S", StringComparison.InvariantCultureIgnoreCase));
-        }
-    }
+    
 }
